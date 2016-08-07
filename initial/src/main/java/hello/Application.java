431c9a15f087d9c8a0f9c847b6e5d7a865bf7dfd
surya-kumar <surya.kumar@flipkart.com>
2016-08-07 19:34:59 +0530

    api

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -3,10 +3,12 @@ package hello;
 import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
     
     public static void main(String[] args) {
--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -1,14 +1,47 @@
 package hello;
 
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import redis.clients.jedis.Jedis;
+
+
+import java.util.*;
+
 
 @RestController
+@RequestMapping("/vnfs")
 public class HelloController {
+
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
     
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @ResponseBody
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public Map<String, Map<String, String>> getVnfDetails() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Map<String, String>> map = new HashMap<>();
+        try {
+            Jedis jedis = new Jedis("localhost");
+            Set<String> names = jedis.keys("*");
+            java.util.Iterator<String> it = names.iterator();
+            while (it.hasNext()) {
+                String s = it.next();
+                Map<String, String> data = objectMapper.readValue(jedis.get(s), new TypeReference<Map<String, String>>() {
+                });
+                map.put(s, data);
+
+            }
+        }catch (Exception e){
+            logger.error("Could not parse redis data");
+        }
+
+        return map;
     }
-    
+
 }
